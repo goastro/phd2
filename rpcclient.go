4f79c9c6aa31d5dc1baecb3f56265a04acc9a83a
Rick Bassham <brodrick.bassham@gmail.com>
2019-01-14 06:24:54 -0500

    Add the rest of the RPC methods

--- rpcclient.go
+++ rpcclient.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"net"
 	"sync"
+
+	"github.com/pkg/errors"
 )
 
 type RPCClient struct {
@@ -225,32 +227,9 @@ type RPCError struct {
 }
 
 type RPCResponse struct {
-	ID    int      `json:"id"`
-	Error RPCError `json:"error,omitempty"`
-}
-
-type EmptyResponse struct {
-	RPCResponse
-}
-
-type StringResponse struct {
-	RPCResponse
-	Result string `json:"result"`
-}
-
-type IntResponse struct {
-	RPCResponse
-	Result int `json:"result"`
-}
-
-type BooleanResponse struct {
-	RPCResponse
-	Result bool `json:"result"`
-}
-
-type FloatSliceResponse struct {
-	RPCResponse
-	Result []float64 `json:"result"`
+	ID     int             `json:"id"`
+	Error  RPCError        `json:"error,omitempty"`
+	Result json.RawMessage `json:"result"`
 }
 
 func (c *RPCClient) processReadLoop() {
@@ -298,7 +277,7 @@ type RPCRequest struct {
 	Params []interface{} `json:"params,omitempty"`
 }
 
-func (c *RPCClient) call(name string, params []interface{}, resp interface{}) error {
+func (c *RPCClient) call(name string, params []interface{}, result interface{}) (*RPCResponse, error) {
 	c.methodMutex.Lock()
 	defer c.methodMutex.Unlock()
 
@@ -312,52 +291,56 @@ func (c *RPCClient) call(name string, params []interface{}, resp interface{}) er
 
 	bytes, err := json.Marshal(&req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	_, err = c.writer.Write(bytes)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	_, err = c.writer.WriteString("\r\n")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = c.writer.Flush()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	line := <-c.methodResponse
 
 	println(line)
 
+	var resp RPCResponse
+
 	err = json.Unmarshal([]byte(line), &resp)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	err = json.Unmarshal(resp.Result, result)
+
+	return &resp, err
 }
 
 // https://github.com/OpenPHDGuiding/phd2/wiki/EventMonitoring#available-methods
 // https://github.com/OpenPHDGuiding/phd2/blob/master/event_server.cpp
 
 func (c *RPCClient) GetExposure() (int, error) {
-	var resp IntResponse
-	err := c.call("get_exposure", nil, &resp)
+	var result int
+	_, err := c.call("get_exposure", nil, &result)
 	if err != nil {
 		return 0, err
 	}
 
-	return resp.Result, nil
+	return result, nil
 }
 
 func (c *RPCClient) CaptureSingleFrame(duration int, subframe []int) error {
-	var resp IntResponse
-	err := c.call("capture_single_frame", []interface{}{duration, subframe}, &resp)
+	var result int
+	_, err := c.call("capture_single_frame", []interface{}{duration, subframe}, &result)
 	if err != nil {
 		return err
 	}
@@ -366,8 +349,8 @@ func (c *RPCClient) CaptureSingleFrame(duration int, subframe []int) error {
 }
 
 func (c *RPCClient) ClearCalibration(which string) error {
-	var resp IntResponse
-	err := c.call("clear_calibration", []interface{}{which}, &resp)
+	var result int
+	_, err := c.call("clear_calibration", []interface{}{which}, &result)
 	if err != nil {
 		return err
 	}
@@ -382,8 +365,8 @@ type Settle struct {
 }
 
 func (c *RPCClient) Dither(pixels float64, raOnly bool, settle Settle) error {
-	var resp IntResponse
-	err := c.call("dither", []interface{}{pixels, raOnly, settle}, &resp)
+	var result int
+	_, err := c.call("dither", []interface{}{pixels, raOnly, settle}, &result)
 	if err != nil {
 		return err
 	}
@@ -392,18 +375,18 @@ func (c *RPCClient) Dither(pixels float64, raOnly bool, settle Settle) error {
 }
 
 func (c *RPCClient) FindStar() ([]float64, error) {
-	var resp FloatSliceResponse
-	err := c.call("find_star", nil, &resp)
+	var result []float64
+	_, err := c.call("find_star", nil, &result)
 	if err != nil {
 		return nil, err
 	}
 
-	return resp.Result, nil
+	return result, nil
 }
 
 func (c *RPCClient) FlipCalibration() error {
-	var resp IntResponse
-	err := c.call("flip_calibration", nil, &resp)
+	var result int
+	_, err := c.call("flip_calibration", nil, &result)
 	if err != nil {
 		return err
 	}
@@ -412,163 +395,285 @@ func (c *RPCClient) FlipCalibration() error {
 }
 
 func (c *RPCClient) GetAppState() (string, error) {
-	var resp StringResponse
-	err := c.call("get_app_state", nil, &resp)
+	var result string
+	_, err := c.call("get_app_state", nil, &result)
 	if err != nil {
 		return "", err
 	}
 
-	return resp.Result, nil
+	return result, nil
 }
 
 func (c *RPCClient) GetCalibrated() (bool, error) {
-	var resp BooleanResponse
-	err := c.call("get_calibrated", nil, &resp)
+	var result bool
+	_, err := c.call("get_calibrated", nil, &result)
 	if err != nil {
 		return false, err
 	}
 
-	return resp.Result, nil
+	return result, nil
 }
 
 func (c *RPCClient) GetConnected() (bool, error) {
-	var resp BooleanResponse
-	err := c.call("get_connected", nil, &resp)
+	var result bool
+	_, err := c.call("get_connected", nil, &result)
 	if err != nil {
 		return false, err
 	}
 
-	return resp.Result, nil
+	return result, nil
 }
 
 func (c *RPCClient) GetAlgorithmParamNames(axis string) ([]string, error) {
-	return nil, ErrNotImplemented
+	var result []string
+	_, err := c.call("get_algo_param_names", []interface{}{axis}, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
-func (c *RPCClient) GetAlgorithmParam(axis, param string) (string, error) {
-	return "", ErrNotImplemented
+func (c *RPCClient) GetAlgorithmParam(axis, param string) (float64, error) {
+	var result float64
+	_, err := c.call("get_algo_param", []interface{}{axis, param}, &result)
+	if err != nil {
+		return 0, err
+	}
+
+	return result, nil
 }
 
-func (c *RPCClient) GetCalibrationData(which string) error {
-	return ErrNotImplemented
+type CalibrationData struct {
+	Calibrated bool    `json:"calibrated"`
+	XAngle     float64 `json:"xAngle"`
+	XRate      float64 `json:"xRate"`
+	XParity    string  `json:"xParity"`
+	YAngle     float64 `json:"yAngle"`
+	YRate      float64 `json:"yRate"`
+	YParity    string  `json:"yParity"`
 }
 
-func (c *RPCClient) GetCoolerStatus() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetCalibrationData(which string) (CalibrationData, error) {
+	var result CalibrationData
+	_, err := c.call("get_calibration_data", []interface{}{which}, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetCurrentEquipment() error {
-	return ErrNotImplemented
+type CoolerStatus struct {
+	Temperature float64 `json:"temperature"`
+	CoolerOn    bool    `json:"coolerOn"`
+	Setpoint    float64 `json:"setpoint"`
+	Power       float64 `json:"power"`
 }
 
-func (c *RPCClient) GetDecGuidMode() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetCoolerStatus() (CoolerStatus, error) {
+	var result CoolerStatus
+	_, err := c.call("get_cooler_status", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetExposureDurations() error {
-	return ErrNotImplemented
+type Equipment struct {
+	Name      string `json:"name"`
+	Connected bool   `json:"connected"`
 }
 
-func (c *RPCClient) GetLockPosition() error {
-	return ErrNotImplemented
+type CurrentEquipment struct {
+	Camera   Equipment `json:"camera"`
+	Mount    Equipment `json:"mount"`
+	AuxMount Equipment `json:"aux_mount"`
+	AO       Equipment `json:"AO"`
+	Rotator  Equipment `json:"rotator"`
 }
 
-func (c *RPCClient) GetLockShiftEnabled() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetCurrentEquipment() (CurrentEquipment, error) {
+	var result CurrentEquipment
+	_, err := c.call("get_algo_param", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetLockShiftParams() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetDecGuidMode() (string, error) {
+	var result string
+	_, err := c.call("get_dec_guide_mode", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetPaused() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetExposureDurations() ([]int, error) {
+	var result []int
+	_, err := c.call("get_exposure_durations", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetPixelScale() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetLockPosition() ([]int, error) {
+	var result []int
+	_, err := c.call("get_lock_position", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetProfile() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetLockShiftEnabled() (bool, error) {
+	var result bool
+	_, err := c.call("get_lock_shift_enabled", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetProfiles() error {
-	return ErrNotImplemented
+type LockShiftParams struct {
+	Enabled bool      `json:"enabled"`
+	Rate    []float64 `json:"rate"`
+	Units   string    `json:"units"`
+	Axes    string    `json:"axes"`
 }
 
-func (c *RPCClient) GetSearchRegion() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetLockShiftParams() (LockShiftParams, error) {
+	var result LockShiftParams
+	_, err := c.call("get_lock_shift_params", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GetSensorTemperature() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetPaused() (bool, error) {
+	var result bool
+	_, err := c.call("get_paused", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
+}
+
+func (c *RPCClient) GetPixelScale() (float64, error) {
+	var result float64
+	_, err := c.call("get_pixel_scale", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
+}
+
+type Profile struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (c *RPCClient) GetProfile() (Profile, error) {
+	var result Profile
+	_, err := c.call("get_profile", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
+}
+
+func (c *RPCClient) GetProfiles() ([]Profile, error) {
+	var result []Profile
+	_, err := c.call("get_profiles", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
+}
+
+func (c *RPCClient) GetSearchRegion() (int, error) {
+	var result int
+	_, err := c.call("get_search_region", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
+}
+
+func (c *RPCClient) GetSensorTemperature() (float64, error) {
+	var result float64
+	_, err := c.call("get_sensor_temperature", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
 func (c *RPCClient) GetStarImage() error {
 	return ErrNotImplemented
 }
 
-func (c *RPCClient) GetUseSubframes() error {
-	return ErrNotImplemented
+func (c *RPCClient) GetUseSubframes() (bool, error) {
+	var result bool
+	_, err := c.call("get_use_subframes", nil, &result)
+	return result, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) Guide() error {
-	return ErrNotImplemented
+func (c *RPCClient) Guide(settle Settle, recalibrate bool) error {
+	var result int
+	_, err := c.call("guide", []interface{}{settle, recalibrate}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) GuidePulse() error {
-	return ErrNotImplemented
+func (c *RPCClient) GuidePulse(amount int, direction, which string) error {
+	var result int
+	_, err := c.call("guide_pulse", []interface{}{amount, direction, which}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
 func (c *RPCClient) Loop() error {
-	return ErrNotImplemented
+	var result int
+	_, err := c.call("loop", nil, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SaveImage() error {
-	return ErrNotImplemented
+func (c *RPCClient) SaveImage() (string, error) {
+	var result struct {
+		Filename string `json:"filename"`
+	}
+	_, err := c.call("save_image", nil, &result)
+	return result.Filename, errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetAlgorithmParam() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetAlgorithmParam(axis, name string, value float64) error {
+	var result int
+	_, err := c.call("set_algo_param", []interface{}{axis, name, value}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetConnected() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetConnected(connect bool) error {
+	var result int
+	_, err := c.call("set_algo_param", []interface{}{connect}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetDecGuideMode() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetDecGuideMode(mode string) error {
+	var result int
+	_, err := c.call("set_dec_guide_mode", []interface{}{mode}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetExposure() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetExposure(length int) error {
+	var result int
+	_, err := c.call("set_exposure", []interface{}{length}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetLockPosition() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetLockPosition(x, y float64, exact bool) error {
+	var result int
+	_, err := c.call("set_lock_position", []interface{}{x, y, exact}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetLockShiftEnabled() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetLockShiftEnabled(enable bool) error {
+	var result int
+	_, err := c.call("set_lock_shift_enabled", []interface{}{enable}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetLockShiftParams() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetLockShiftParams(params LockShiftParams) error {
+	var result int
+	_, err := c.call("set_lock_shift_params", []interface{}{params}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetPaused() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetPaused(paused, full bool) error {
+	var result int
+
+	params := []interface{}{paused}
+	if full {
+		params = append(params, "full")
+	}
+
+	_, err := c.call("set_paused", params, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
-func (c *RPCClient) SetProfile() error {
-	return ErrNotImplemented
+func (c *RPCClient) SetProfile(id int) error {
+	var result int
+	_, err := c.call("set_profile", []interface{}{id}, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
 func (c *RPCClient) Shutdown() error {
-	return ErrNotImplemented
+	var result int
+	_, err := c.call("shutdown", nil, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
 
 func (c *RPCClient) StopCapture() error {
-	return ErrNotImplemented
+	var result int
+	_, err := c.call("stop_capture", nil, &result)
+	return errors.Wrap(err, "error calling jsonrpc method")
 }
